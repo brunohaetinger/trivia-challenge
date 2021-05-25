f03412ba04614ce0f38d0e9722f144895934d155
Bruno Haetinger <brunohaetinger@gmail.com>
2021-05-24 23:00:29 -0300

    Create SimplpeButton component

---
+++ src/components/SimpleButton.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const SimpleButton = styled.button`
+  height: 45px;
+  min-width: 80px;
+  background-color: transparent;
+  border-radius: 10px;
+  border-color: transparent;
+
+  &:hover {
+    background-color: #9e9e9e;
+  }
+`;
+
+export default SimpleButton;
